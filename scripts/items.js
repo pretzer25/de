7f66621b6d9202bff2455f0a54734823716950d5
pretzer25 <153140996+pretzer25@users.noreply.github.com>
2023-12-30 21:34:55 -0600

    Delete scripts/items.js

--- scripts/items.js
+++
@@ -1,6 +0,0 @@
-export const items = [
-  { name: "Item 1", imageUrl: "https://example.com/image1.jpg", link: "https://example.com/link1" },
-  { name: "Item 2", imageUrl: "https://example.com/image2.jpg", link: "https://example.com/link2" },
-  { name: "Item 3", imageUrl: "https://example.com/image3.jpg", link: "https://example.com/link3" },
-  // Add more items as needed
-];
