4dee2dbe933bca392fc715fdbb70cf4770223681
pretzer25 <153140996+pretzer25@users.noreply.github.com>
2023-12-30 21:35:02 -0600

    Delete scripts/main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-
-function download() {
-  // Create a temporary link for download
-  const link = document.createElement("a");
-  link.href = "/./files/fort.html"; // Replace with your actual file URL
-  link.download = "study.html"; // Adjust the filename if needed
-  link.click();
-
-
